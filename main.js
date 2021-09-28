379a0114562febb6c3700c317f96cf12e3b0e5b4
ishaanpunugupati <86003111+ishaanpunugupati@users.noreply.github.com>
2021-09-28 17:51:40 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,47 @@
+function setup() {
+    canvas = createCanvas(350, 350);
+    canvas.center();
+    background("white");
+    canvas.mouseReleased(classifyCanvas);
+    synth = window.speechSynthesis;
+}
+
+function preload() {
+
+
+    classifier = ml5.imageClassifier('DoodleNet');
+}
+
+
+
+function clearCanvas() {
+
+    background("white");
+}
+
+function draw() {
+    strokeWeight(13);
+    stroke(0);
+    if (mouseIsPressed) {
+        line(pmouseX, pmouseY, mouseX, mouseY);
+    }
+}
+
+function classifyCanvas() {
+     classifier.classify(canvas, gotResult);
+}
+
+function gotResult(error, result) {
+    if(error) {
+        console.error(error);
+    }
+    else {
+    console.log(result);
+    document.getElementById('label').innerHTML = 'Label: ' + result[0].label;
+
+    document.getElementById('confidence').innerHTML = 'Confidence: ' +Math.round(result[0].confidence * 100) + '%';
+
+    utterThis = new SpeechSynthesisUtterance(result[0].label);
+    synth.speak(utterThis);
+}
+}
